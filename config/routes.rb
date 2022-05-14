04a88fb494bc35bf5d13d44dfd21c72bafff9fcb
suguru nakagawa <s.nakagawa.n4s3@gmail.com>
2022-05-14 20:06:33 +0900

    Add: likes_controllerを作成
    createアクションを作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth', controllers: {
     registrations: 'auth/registrations'
   }
-  resources :messages, only: %i[index]
+  resources :messages, only: %i[index] do
+    member do
+      resources :likes, only: %i[create]
+    end
+  end
 end
